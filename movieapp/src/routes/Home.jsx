10c2343d4c601ed88c400d607975340c6885797f
Mi-Kyung331 <uu.3120@gmail.com>
2025-03-27 20:12:04 +0900

    feat: 20250327 movie info app finished

--- movieapp/src/routes/Home.jsx
+++ movieapp/src/routes/Home.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
+import"./Home.css";
 import Movie from "../components/movie";
 
-function Home() {
+export default function Home() {
   const [ loading, setLoading ] = useState(true);
   const [ movies, setMovies ] = useState([]);
-  
+
   const getMovies = async() => {
     const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year');
     const json = await response.json();
@@ -16,24 +17,25 @@ function Home() {
     getMovies();
   }, []);
 
-  return(
-    <div>
-      {loading ? <h1>Loading...</h1> : <div>{movies.map(movie =>
-        <Movie 
-          key={movie.id}
-          id={movie.id}
-          coverImg={movie.medium_cover_image}
-          title={movie.title}
-          summary={movie.summary}
-          genres={movie.genres}
-        />
-      )}</div>
-    }
-
-    </div>
-
+  return (
+    <div className="home-container">
+      { loading ? (
+        <h1 className="home-loading">로딩 중...</h1>
+      ): (
+        <div className="movie-grid">
+          {movies.map((movie) => (
+            <Movie 
+              key={movie.id}
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
     
+    </div>
   )
 }
-
-export default Home;
